6718ac6ca418118d175ecc1ee6d909719537c755
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2021-08-08 18:49:18 +0530

    Add files via upload

---
+++ 10 Days of JavaScript (NodeJs)/Day_06_JavaScript_Dates.js
@@ -0,0 +1,63 @@
+// Task
+
+// Given a date string, dateString, in the format MM/DD/YYYY, find and return the day name for that date. Each day name must be one of the following strings: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. For example, the day name for the date 12/07/2016 is Wednesday.
+
+// Input Format
+// Locked stub code in the editor reads the following input from stdin:
+// The first line contains an integer, d, denoting the number of dates to check.
+// Each line i of the d subsequent lines contains a date in MM/DD/YYYY format; each date denotes some dateString that is passed to the function.
+
+// Output Format
+// The function must return a string denoting the day of the week corresponding to the date denoted by dateString.
+
+// Sample Input 0
+// 2
+// 10/11/2009
+// 11/10/2010
+
+// Sample Output 0
+// Sunday
+// Wednesday
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // Write your code here
+    let day = new Date(dateString).getDay();
+    return dayName[day];
+}
+
+
+function main() {
+    const d = +(readLine());
+
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+
+        console.log(getDayName(date));
+    }
+}
---
+++ 10 Days of JavaScript (NodeJs)/Day_07_Regular_Experssions_I.js
@@ -0,0 +1,56 @@
+// Task
+
+// Complete the function in the editor below by returning a RegExp object, re, that matches any string s that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u.
+
+// Output Format
+// The function must return a RegExp object that matches any string  beginning with and ending in the same vowel.
+
+// Sample Input 0
+// bcd
+
+// Sample Output 0
+// false
+
+// Explanation 0
+// This string starts with (and ends in) a consonant, so it cannot start and end with the same vowel.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    const re = /^([aeiou]).*\1$/;
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+
+    console.log(re.test(s));
+}
