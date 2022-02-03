0fa1ba9ddf92cb36be05afac2ed6c1a4e91be379
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:01:06 +0530

    Update Day_03_Arrays.js

--- 10 Days of JavaScript (NodeJs)/Day_03_Arrays.js
+++ 10 Days of JavaScript (NodeJs)/Day_03_Arrays.js
@@ -1,23 +1,23 @@
-// Function Description
+/*Function Description
 
-// Complete the getSecondLargest function.
+Complete the getSecondLargest function.
 
-// getSecondLargest has the following parameters:
-// int nums[n]: an array of integers
+getSecondLargest has the following parameters:
+int nums[n]: an array of integers
 
-// Returns
-// int: the second largest number in nums
+Returns
+int: the second largest number in nums
 
-// Input Format
-// The first line contains an integer, n, the size of the nums array.
-// The second line contains n space-separated numbers that describe the elements in nums.
+Input Format
+The first line contains an integer, n, the size of the nums array.
+The second line contains n space-separated numbers that describe the elements in nums.
 
-// Sample Input 0
-// 5
-// 2 3 6 6 5
+Sample Input 0
+5
+2 3 6 6 5
 
-// Sample Output 0
-// 5
+Sample Output 0
+5       */
 
 'use strict';
 
