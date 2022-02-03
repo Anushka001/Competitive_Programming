784f66c6a15f541c6b1d92d36442c4e8327f5ee6
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:06:34 +0530

    Update Day_00_Data_Types.js

--- 10 Days of JavaScript (NodeJs)/Day_00_Data_Types.js
+++ 10 Days of JavaScript (NodeJs)/Day_00_Data_Types.js
@@ -1,20 +1,20 @@
-// Task
+/* Task
 
-// Variables named firstInteger, firstDecimal, and firstString are declared. You must use the '+' operator to perform the following sequence of operations:
+Variables named firstInteger, firstDecimal, and firstString are declared. You must use the '+' operator to perform the following sequence of operations:
 
-// 1. Convert secondInteger to an integer (Number type), then sum it with firstInteger and print the result on a new line using console.log.
-// 2. Convert secondDecimal to a floating-point number (Number type), then sum it with firstDecimal and print the result on a new line using console.log.
-// 3. Print the concatenation of firstString and secondString on a new line using console.log. Note that firstString must be printed first.
+1. Convert secondInteger to an integer (Number type), then sum it with firstInteger and print the result on a new line using console.log.
+2. Convert secondDecimal to a floating-point number (Number type), then sum it with firstDecimal and print the result on a new line using console.log.
+3. Print the concatenation of firstString and secondString on a new line using console.log. Note that firstString must be printed first.
 
-// Sample Input 0
-// 12
-// 4.32
-// is the best place to learn and practice coding!
+Sample Input 0
+12
+4.32
+is the best place to learn and practice coding!
 
-// Sample Output 0
-// 16
-// 8.32
-// HackerRank is the best place to learn and practice coding!
+Sample Output 0
+16
+8.32
+HackerRank is the best place to learn and practice coding!   */
 
 'use strict';
 
