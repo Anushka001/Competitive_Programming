1806c7134bdbad5344eae2d02d5d090f39eb3d72
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:18:50 +0530

    Update For_Loops_in_C.c

--- C-language/For_Loops_in_C.c
+++ C-language/For_Loops_in_C.c
@@ -1,16 +1,15 @@
-// Task
+/* Task
+For each integer n in the interval [a,b] (given as input) :
+If 1 <= n <= 9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+Else if n>9 and it is an even number, then print "even".
+Else if n>9 and it is an odd number, then print "odd".
 
-// For each integer n in the interval [a,b] (given as input) :
-// If 1 <= n <= 9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
-// Else if n>9 and it is an even number, then print "even".
-// Else if n>9 and it is an odd number, then print "odd".
+Input Format
+The first line contains an integer, a.
+The seond line contains an integer, b.
 
-// Input Format
-// The first line contains an integer, a.
-// The seond line contains an integer, b.
-
-// Output Format
-// Print the appropriate English representation,even, or odd, based on the conditions described in the 'task' section.
+Output Format
+Print the appropriate English representation,even, or odd, based on the conditions described in the 'task' section.  */
 
 #include <stdio.h>
 #include <string.h>
