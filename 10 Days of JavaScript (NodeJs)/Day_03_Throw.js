3c845341d3fab60e03c5b428d030cc7fa56197c6
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:00:17 +0530

    Update Day_03_Throw.js

--- 10 Days of JavaScript (NodeJs)/Day_03_Throw.js
+++ 10 Days of JavaScript (NodeJs)/Day_03_Throw.js
@@ -1,20 +1,19 @@
-// Task
+/*Task
+Complete the isPositive function below. It has one integer parameter, a. If the value of a is positive, it must return the string YES. Otherwise, it must throw an Error according to the following rules:
 
-// Complete the isPositive function below. It has one integer parameter, a. If the value of a is positive, it must return the string YES. Otherwise, it must throw an Error according to the following rules:
+If a is 0, throw an Error with message = Zero Error.
+If a is negative, throw an Error with message = Negative Error.
 
-// If a is 0, throw an Error with message = Zero Error.
-// If a is negative, throw an Error with message = Negative Error.
+Sample Input 0
+3
+1
+2
+3
 
-// Sample Input 0
-// 3
-// 1
-// 2
-// 3
-
-// Sample Output 0
-// YES
-// YES
-// YES
+Sample Output 0
+YES
+YES
+YES     */
 
 'use strict';
 
