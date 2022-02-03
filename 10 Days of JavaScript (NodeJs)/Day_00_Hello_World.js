28532e398734105e1f93670defe36c3df825ce9a
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:05:59 +0530

    Update Day_00_Hello_World.js

--- 10 Days of JavaScript (NodeJs)/Day_00_Hello_World.js
+++ 10 Days of JavaScript (NodeJs)/Day_00_Hello_World.js
@@ -1,16 +1,16 @@
-// // Task
+/* Task
 
-// // A greeting function is provided for you in the editor below. It has one parameter, parameterVariable. Perform the following tasks to complete this challenge:
+A greeting function is provided for you in the editor below. It has one parameter, parameterVariable. Perform the following tasks to complete this challenge:
 
-// // Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
-// // Use console.log() to print the contents of parameterVariable (i.e., the argument passed to main).
+Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+Use console.log() to print the contents of parameterVariable (i.e., the argument passed to main).
 
-// // Sample Input 0
-// // Welcome to 10 Days of JavaScript!
+Sample Input 0
+Welcome to 10 Days of JavaScript!
 
-// // Sample Output 0
-// // Hello, World!
-// // Welcome to 10 Days of JavaScript!
+Sample Output 0
+Hello, World!
+Welcome to 10 Days of JavaScript! */
 
 'use strict';
 
