9d6cff4ef0801d866017f9c2443fa18653768548
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:11:04 +0530

    Update Array_Reversal_in_C.c

--- C-language/Array_Reversal_in_C.c
+++ C-language/Array_Reversal_in_C.c
@@ -1,11 +1,11 @@
-// Task
-// Given an array, of size n, reverse it.
+/* Task
+Given an array, of size n, reverse it.
 
-// Input Format
-// The first line contains an integer, n, denoting the size of the array. The next line contains n space-separated integers denoting the elements of the array.
+Input Format
+The first line contains an integer, n, denoting the size of the array. The next line contains n space-separated integers denoting the elements of the array.
 
-// Output Format
-// If array, arr = [1,2,3,4,5] , after reversing it, the array should be, arr = [5,4,3,2,1].
+Output Format
+If array, arr = [1,2,3,4,5] , after reversing it, the array should be, arr = [5,4,3,2,1].    */
 
 
 #include <stdio.h>
