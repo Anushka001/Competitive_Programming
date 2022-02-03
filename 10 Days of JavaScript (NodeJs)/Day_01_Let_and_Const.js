97e5e34349277741f079c4c8759508a7a18cfcef
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:04:31 +0530

    Update Day_01_Let_and_Const.js

--- 10 Days of JavaScript (NodeJs)/Day_01_Let_and_Const.js
+++ 10 Days of JavaScript (NodeJs)/Day_01_Let_and_Const.js
@@ -1,9 +1,9 @@
-// Task
+/* Task
 
-// Declare a constant variable, PI, and assign it the value Math.PI. You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
-// Read a number, r, denoting the radius of a circle from stdin.
-// Use PI and r to calculate the area and perimeter of a circle having radius r.
-// Print area as the first line of output and print perimeter as the second line of output.
+Declare a constant variable, PI, and assign it the value Math.PI. You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
+Read a number, r, denoting the radius of a circle from stdin.
+Use PI and r to calculate the area and perimeter of a circle having radius r.
+Print area as the first line of output and print perimeter as the second line of output. */
 
 'use strict';
 
