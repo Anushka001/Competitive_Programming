49a2c5c468e616936ed00101381d148927f5d39f
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:16:13 +0530

    Update Digit_Frequency.c

--- C-language/Digit_Frequency.c
+++ C-language/Digit_Frequency.c
@@ -1,11 +1,11 @@
-// Task
-// Given a string, s, consisting of alphabets and digits, find the frequency of each digit in the given string.
+/* Task
+Given a string, s, consisting of alphabets and digits, find the frequency of each digit in the given string.
 
-// Input Format
-// The first line contains a string, num which is the given number.
+Input Format
+The first line contains a string, num which is the given number.
 
-// Output Format
-// Print ten space-separated integers in a single line denoting the frequency of each digit from 0 to 9.
+Output Format
+Print ten space-separated integers in a single line denoting the frequency of each digit from 0 to 9.    */
 
 #include <stdio.h>
 #include <string.h>
