988521f792796224d631596062aa8a0a496c3475
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:02:11 +0530

    Update Day_02_Loops.js

--- 10 Days of JavaScript (NodeJs)/Day_02_Loops.js
+++ 10 Days of JavaScript (NodeJs)/Day_02_Loops.js
@@ -1,33 +1,33 @@
-// Task
+/*Task
 
-// First, print each vowel in s on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in s.
-// Second, print each consonant (i.e., non-vowel) in s on a new line in the same order as it appeared in s.
+First, print each vowel in s on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in s.
+Second, print each consonant (i.e., non-vowel) in s on a new line in the same order as it appeared in s.
 
-// Input Format
-// There is one line of input with the string s.
+Input Format
+There is one line of input with the string s.
 
-// Output Format
-// First, print each vowel in s on a new line (in the same order as they appeared in s). Second, print each consonant (i.e., non-vowel) in s on a new line (in the same order as they appeared in s).
+Output Format
+First, print each vowel in s on a new line (in the same order as they appeared in s). Second, print each consonant (i.e., non-vowel) in s on a new line (in the same order as they appeared in s).
 
-// Sample Input 0
-// javascriptloops
+Sample Input 0
+javascriptloops
 
-// Sample Output 0
-// a
-// a
-// i
-// o
-// o
-// j
-// v
-// s
-// c
-// r
-// p
-// t
-// l
-// p
-// s
+Sample Output 0
+a
+a
+i
+o
+o
+j
+v
+s
+c
+r
+p
+t
+l
+p
+s   */
 
 'use strict';
 
