dc35bb0dfd3c995ba2aa385c92502dffc15d3cd3
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:14:47 +0530

    Update Boxes_Through_a_Tunnel.c

--- C-language/Boxes_Through_a_Tunnel.c
+++ C-language/Boxes_Through_a_Tunnel.c
@@ -1,14 +1,13 @@
-// Task
-// You are transporting some boxes through a tunnel, where each box is a parallelepiped, and is characterized by its length, width and height.
+/* Task
+You are transporting some boxes through a tunnel, where each box is a parallelepiped, and is characterized by its length, width and height.
+The height of the tunnel 41 feet and the width can be assumed to be infinite. A box can be carried through the tunnel only if its height is strictly less than the tunnel's height. Find the volume of each box that can be successfully transported to the other end of the tunnel. Note: Boxes cannot be rotated.
 
-// The height of the tunnel 41 feet and the width can be assumed to be infinite. A box can be carried through the tunnel only if its height is strictly less than the tunnel's height. Find the volume of each box that can be successfully transported to the other end of the tunnel. Note: Boxes cannot be rotated.
+Input Format
+The first line contains a single integer n, denoting the number of boxes.
+n lines follow with three integers on each separated by single spaces lengthi, widthi and heighti which are length, width and height in feet of the i-th box.
 
-// Input Format
-// The first line contains a single integer n, denoting the number of boxes.
-// n lines follow with three integers on each separated by single spaces lengthi, widthi and heighti which are length, width and height in feet of the i-th box.
-
-// Output Format
-// For every box from the input which has a height lesser than 41 feet, print its volume in a separate line.
+Output Format
+For every box from the input which has a height lesser than 41 feet, print its volume in a separate line.	*/
 
 #include <stdio.h>
 #include <stdlib.h>
