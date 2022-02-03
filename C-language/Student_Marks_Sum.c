35abbdeaa75179f1b8847e146c8122251d454b14
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:09:14 +0530

    Update Student_Marks_Sum.c

--- C-language/Student_Marks_Sum.c
+++ C-language/Student_Marks_Sum.c
@@ -1,28 +1,27 @@
-// Task
-// You are given an array of integers, marks, denoting the marks scored by students in a class.
+/* Task
+You are given an array of integers, marks, denoting the marks scored by students in a class.
 
-// The alternating elements marks0, marks2, marks4 and so on denote the marks of boys.
-// Similarly, marks1, marks3, marks5 and so on denote the marks of girls.
+The alternating elements marks0, marks2, marks4 and so on denote the marks of boys.
+Similarly, marks1, marks3, marks5 and so on denote the marks of girls.
 
-// The array name, marks, works as a pointer which stores the base address of that array. In other words, marks contains the address where marks0 is stored in the memory.
+The array name, marks, works as a pointer which stores the base address of that array. In other words, marks contains the address where marks0 is stored in the memory.
 
-// Function Description
+Function Description
+Complete the function, marks_summation in the editor below.
 
-// Complete the function, marks_summation in the editor below.
+marks_summation has the following parameters:
 
-// marks_summation has the following parameters:
+int marks[number_of_students]: the marks for each student
+int number_of_students: the size of marks[]
+char gender: either 'g' or 'b'
 
-// int marks[number_of_students]: the marks for each student
-// int number_of_students: the size of marks[]
-// char gender: either 'g' or 'b'
-// Returns
+Returns
+int: the sum of marks for boys if gender = b, or of marks of girls if gender = g
 
-// int: the sum of marks for boys if gender = b, or of marks of girls if gender = g
-
-// Input Format
-// The first line contains number_of_students, denoting the number of students in the class, hence the number of elements in marks.
-// Each of the number_of_students subsequent lines contains marksi.
-// The next line contains gender.
+Input Format
+The first line contains number_of_students, denoting the number of students in the class, hence the number of elements in marks.
+Each of the number_of_students subsequent lines contains marksi.
+The next line contains gender. */
 
 #include <stdio.h>
 #include <string.h>
