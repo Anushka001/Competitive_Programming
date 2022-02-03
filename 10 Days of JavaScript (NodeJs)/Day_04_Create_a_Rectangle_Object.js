3f6ccb2b5669d05de5ca7dbb3543e240a6996d46
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 16:45:37 +0530

    Update Day_04_Create_a_Rectangle_Object.js

--- 10 Days of JavaScript (NodeJs)/Day_04_Create_a_Rectangle_Object.js
+++ 10 Days of JavaScript (NodeJs)/Day_04_Create_a_Rectangle_Object.js
@@ -1,26 +1,25 @@
-// Task
+/*Task
+Complete the function in the editor. It has two parameters: a and b. It must return an object modeling a rectangle that has the following properties:
 
-// Complete the function in the editor. It has two parameters: a and b. It must return an object modeling a rectangle that has the following properties:
+length: This value is equal to a.
+width: This value is equal to b.
+perimeter: This value is equal to 2.(a+b)
+area: This value is equal to a.b
 
-// length: This value is equal to a.
-// width: This value is equal to b.
-// perimeter: This value is equal to 2.(a+b)
-// area: This value is equal to a.b
+Note: The names of the object's properties must be spelled correctly to pass this challenge.
 
-// Note: The names of the object's properties must be spelled correctly to pass this challenge.
+Sample Input 0
+4
+5
 
-// Sample Input 0
-// 4
-// 5
+Sample Output 0
+4
+5
+18
+20
 
-// Sample Output 0
-// 4
-// 5
-// 18
-// 20
-
-// Explanation 0
-// Given a length of a = 4 and a width of b = 5, the Rectangle object's perimeter is 4+4+5+5 = 18 and its area is 4.5 = 20.
+Explanation 0
+Given a length of a = 4 and a width of b = 5, the Rectangle object's perimeter is 4+4+5+5 = 18 and its area is 4.5 = 20. */
 
 'use strict';
 
