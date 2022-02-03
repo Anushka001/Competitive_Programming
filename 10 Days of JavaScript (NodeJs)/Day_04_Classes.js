72f9f505e9bc66d02a6b986bc4f3bd2d6b4555f9
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 16:47:06 +0530

    Update Day_04_Classes.js

--- 10 Days of JavaScript (NodeJs)/Day_04_Classes.js
+++ 10 Days of JavaScript (NodeJs)/Day_04_Classes.js
@@ -1,16 +1,13 @@
-// Task
+/* Task
+Create a Polygon class that has the following properties:
 
-// Create a Polygon class that has the following properties:
-
-// - A constructor that takes an array of integer values describing the lengths of the polygon's sides.
-// - A perimeter() method that returns the polygon's perimeter.
+- A constructor that takes an array of integer values describing the lengths of the polygon's sides.
+- A perimeter() method that returns the polygon's perimeter.
 
 
-/*
- * Implement a Polygon class with the following properties:
- * 1. A constructor that takes an array of integer side lengths.
- * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
- */
+Implement a Polygon class with the following properties:
+ 1. A constructor that takes an array of integer side lengths.
+ 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.    */
 
 
 class Polygon {
