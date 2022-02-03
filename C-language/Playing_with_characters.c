ee0fc7debee9446a2acbeb5d7c676e4c7279826c
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:14:19 +0530

    Update Playing_with_characters.c

--- C-language/Playing_with_characters.c
+++ C-language/Playing_with_characters.c
@@ -1,16 +1,15 @@
-// Task
-// You have to print the character, 'ch', in the first line. Then print 's' in next line. In the last line print the sentence, 'sen'.
-// Input Format
+/* Task
+You have to print the character, 'ch', in the first line. Then print 's' in next line. In the last line print the sentence, 'sen'.
 
-// First, take a character, 'ch' as input.
-// Then take the string, 's' as input.
-// Lastly, take the sentence 'sen' as input.
+Input Format
+First, take a character, 'ch' as input.
+Then take the string, 's' as input.
+Lastly, take the sentence 'sen' as input.
 
-// Output Format
-
-// Print three lines of output. The first line prints the character, 'ch'.
-// The second line prints the string, 's'.
-// The third line prints the sentence, 'sen'.
+Output Format
+Print three lines of output. The first line prints the character, 'ch'.
+The second line prints the string, 's'.
+The third line prints the sentence, 'sen'.   */
 
 #include <stdio.h>
 #include <string.h>
