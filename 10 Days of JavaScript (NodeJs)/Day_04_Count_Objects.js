3d7db5cbf1a214162b94e1918b29153ae2541b41
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 16:46:26 +0530

    Update Day_04_Count_Objects.js

--- 10 Days of JavaScript (NodeJs)/Day_04_Count_Objects.js
+++ 10 Days of JavaScript (NodeJs)/Day_04_Count_Objects.js
@@ -1,24 +1,23 @@
-// Task
+/* Task
+Complete the function in the editor. It has one parameter: an array, a, of objects. Each object in the array has two integer properties denoted by x and y. The function must return a count of all such objects o in array a that satisfy o.x == o.y.
 
-// Complete the function in the editor. It has one parameter: an array, a, of objects. Each object in the array has two integer properties denoted by x and y. The function must return a count of all such objects o in array a that satisfy o.x == o.y.
+Input Format
+The first line contains an integer denoting n.
+Each of the n subsequent lines contains two space-separated integers describing the values of x and y.
 
-// Input Format
-// The first line contains an integer denoting n.
-// Each of the n subsequent lines contains two space-separated integers describing the values of x and y.
+Output Format
+Return a count of the total number of objects o such that o.x == o.y.
 
-// Output Format
-// Return a count of the total number of objects o such that o.x == o.y.
+Sample Input 0
+5
+1 1
+2 3
+3 3
+3 4
+4 5
 
-// Sample Input 0
-// 5
-// 1 1
-// 2 3
-// 3 3
-// 3 4
-// 4 5
-
-// Sample Output 0
-// 2
+Sample Output 0
+2        */
 
 'use strict';
 
