d6dbbddcdca4fa958e9d843d9928f0deb42774be
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:03:35 +0530

    Update Day_02_Conditional_Statements_If_Else.js

--- 10 Days of JavaScript (NodeJs)/Day_02_Conditional_Statements_If_Else.js
+++ 10 Days of JavaScript (NodeJs)/Day_02_Conditional_Statements_If_Else.js
@@ -1,13 +1,13 @@
-// Task
+/* Task
 
-// Complete the getGrade(score) function in the editor. It has one parameter: an integer, score, denoting the number of points Julia earned on an exam. It must return the letter corresponding to her grade according to the following rules:
+Complete the getGrade(score) function in the editor. It has one parameter: an integer, score, denoting the number of points Julia earned on an exam. It must return the letter corresponding to her grade according to the following rules:
 
-// If 25 < score <= 30, then score = A.
-// If 20 < score <= 25, then score = B.
-// If 15 < score <= 20, then score = C.
-// If 10 < score <= 15, then score = D.
-// If 5 < score <= 10, then score = E.
-// If 0 <= score <= 5, then score = F.
+If 25 < score <= 30, then score = A.
+If 20 < score <= 25, then score = B.
+If 15 < score <= 20, then score = C.
+If 10 < score <= 15, then score = D.
+If 5 < score <= 10, then score = E.
+If 0 <= score <= 5, then score = F.  */
 
 'use strict';
 
