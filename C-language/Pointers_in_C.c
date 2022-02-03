7075f3099bb35aefb5c573f4aae92fd5b6f2d615
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 17:13:46 +0530

    Update Pointers_in_C.c

--- C-language/Pointers_in_C.c
+++ C-language/Pointers_in_C.c
@@ -1,15 +1,14 @@
-// Task
+/* Task
+Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of a to their sum, and b to their absolute difference. There is no return value, and no return statement is needed.
 
-// Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of a to their sum, and b to their absolute difference. There is no return value, and no return statement is needed.
+a' = a+b
+b' = |a-b|
 
-// a' = a+b
-// b' = |a-b|
+Input Format
+The input will contain two integers, a and b, separated by a newline.
 
-// Input Format
-// The input will contain two integers, a and b, separated by a newline.
-
-// Output Format
-// Modify the two values in place and the code stub main() will print their values.
+Output Format
+Modify the two values in place and the code stub main() will print their values. */
 
 #include <stdio.h>
 #include <stdlib.h>
