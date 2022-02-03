78ebf7fd2ab464897c9ec31de307884aa80378e6
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2022-02-03 16:39:26 +0530

    Update Day_07_Regular_Expressions_III.js

--- 10 Days of JavaScript (NodeJs)/Day_07_Regular_Expressions_III.js
+++ 10 Days of JavaScript (NodeJs)/Day_07_Regular_Expressions_III.js
@@ -1,23 +1,22 @@
-// Task
+/* Task
+Complete the function in the editor below by returning a RegExp object, re, that matches every integer in some string s.
 
-// Complete the function in the editor below by returning a RegExp object, re, that matches every integer in some string s.
+Output Format
+The function must return a RegExp object that matches every integer in some string s.
 
-// Output Format
-// The function must return a RegExp object that matches every integer in some string s.
+Sample Input 0
+102, 1948948 and 1.3 and 4.5
 
-// Sample Input 0
-// 102, 1948948 and 1.3 and 4.5
+Sample Output 0
+102
+1948948
+1
+3
+4
+5
 
-// Sample Output 0
-// 102
-// 1948948
-// 1
-// 3
-// 4
-// 5
-
-// Explanation 0
-// When we call match on string  and pass the correct RegExp as our argument, it returns the following array of results: [ '102', '1948948', '1', '3', '4', '5' ].
+Explanation 0
+When we call match on string  and pass the correct RegExp as our argument, it returns the following array of results: [ '102', '1948948', '1', '3', '4', '5' ]. */
 
 'use strict';
 
