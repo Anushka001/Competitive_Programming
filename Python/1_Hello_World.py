6d270814802f8b9b1583176a694632e10d9f4d4d
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2023-01-23 20:32:03 +0530

    Add files via upload

---
+++ Python/1_Hello_World.py
@@ -0,0 +1,11 @@
+'''
+Print string "Hello, World!" in Python
+
+Input Format
+You do not need to read any input in this challenge.
+
+Output Format
+Print Hello, World! to stdout.
+'''
+
+print("Hello, World!")
